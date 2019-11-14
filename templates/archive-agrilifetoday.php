4fea80858cf32bec42a934177ef07b08efde3313
Zachary Watkins <watkinza@gmail.com>
2019-11-14 11:40:41 -0600

    Force search page to full width for AgriLife Today

--- templates/archive-agrilifetoday.php
+++ templates/archive-agrilifetoday.php
@@ -8,6 +8,7 @@
  */
 
 // Remove search result meta data
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 remove_action( 'genesis_entry_content', 'genesis_do_post_permalink', 14 );
 remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
